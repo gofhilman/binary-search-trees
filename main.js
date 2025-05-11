f7a2bf6b795b22a17523952f5bfbf6b6e6458043
Hilman Fikry <hilman12fikry@gmail.com>
2025-05-11 22:21:34 +0700

    feat: add buildTree method to Tree class

---
+++ main.js
@@ -0,0 +1,6 @@
+import Tree  from "./tree-class.js";
+
+const test = new Tree();
+
+test.buildTree([1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]);
+test.prettyPrint();
--- tree-class.js
+++ tree-class.js
@@ -6,6 +6,30 @@ class Tree {
     }
 
     buildTree(array) {
-
+        const tidyArray = [...new Set(array)].sort((a, b) => a - b);
+        return this.root = this.buildBST(tidyArray);
     }
+
+    buildBST(array, start = 0, end = array.length - 1) {
+        if(start > end) return null;
+        const mid = Math.floor((start + end) / 2);
+        const root = new Node(array[mid]);
+        root.left = this.buildBST(array, start, mid - 1);
+        root.right = this.buildBST(array, mid + 1, end);
+        return root;
+    }
+
+    prettyPrint(node = this.root, prefix = "", isLeft = true) {
+        if (node === null) return;
+        if (node.right !== null) {
+            this.prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+        if (node.left !== null) {
+            this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    };
+
 }
+
+export default Tree;
